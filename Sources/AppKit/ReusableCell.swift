f5412cbb72cf4b9602f53548b101edfc36172ec2
Bensalah <amine.bensalah@outlook.com>
2018-03-05 22:04:42 +0100

    .gitignore is now working and add Updatable & macOS files

---
+++ Sources/AppKit/ReusableCell.swift
@@ -0,0 +1,36 @@
+//
+//  Reusable.swift
+//  Instapaperable
+//
+//  Created by Amine Bensalah on 24/07/2017.
+//  Copyright © 2017 Amine Bensalah. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+public protocol ReusableCell: Reusable { }
+
+public extension ReusableCell {
+    static var nib: NSNib? { return nil }
+}
+
+public extension NSTableView {
+    public func registerReusableCell<T: NSTableCellView>(_: T.Type) where T: ReusableCell {
+        self.register(T.nib, forIdentifier: NSUserInterfaceItemIdentifier(rawValue: T.reuseIdentifier))
+    }
+}
+
+public extension NSCollectionView {
+    public func registerReusableCell<T: NSCollectionViewItem>(_: T.Type) where T: ReusableCell {
+        if let nib = T.nib {
+            self.register(nib, forItemWithIdentifier: NSUserInterfaceItemIdentifier(rawValue: T.reuseIdentifier))
+        } else {
+            self.register(T.self, forItemWithIdentifier: NSUserInterfaceItemIdentifier(rawValue: T.reuseIdentifier))
+        }
+    }
+
+    public func makeItemWithIdentifierReusableCell<T: NSCollectionViewItem>(_: T.Type, indexPath: IndexPath) -> T where T: ReusableCell {
+        return self.makeItem(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: T.reuseIdentifier), for: indexPath) as! T
+    }
+}
